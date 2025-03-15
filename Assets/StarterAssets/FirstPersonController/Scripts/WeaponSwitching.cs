306290f9cdcfc850d5736cbb494296824198909a
Rogertag <159093091+Rogertag@users.noreply.github.com>
2025-03-16 10:44:16 +1100

    Weapon swap code

--- Assets/StarterAssets/FirstPersonController/Scripts/WeaponSwitching.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/WeaponSwitching.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponSwitching : MonoBehaviour
 {
+    public int selectedWeapon = 0; // first weapon in order of arrangement
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SelectedWeapon();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SelectWeapon()// when given the choice to the player to enable the weapon wanted
+    { 
+        int i = 0;
+          foreach (Transform weapon in transform)
+          {
+            if (i == selectedWeapon) // check is weapon is active
+                 weapon.gameObject.SetActive(true); // do we want to active chosen weapon?
+            else 
+                 weapon.gameObject.SetActive(false); // check the i varible to enable and disable weapons
+            i++;
+          }
     }
 }
